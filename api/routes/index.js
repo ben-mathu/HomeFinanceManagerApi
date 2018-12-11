a3f0b5c6eba0f5fca6cf3dce3614c57602301b19
Benard Mathu <mathubenard@gmail.com>
2018-12-11 20:39:51 +0300

    Updates

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   var items = require('./financeManagerRoutes');
   var users = require('./financierUserRoutes');
-  app.use('/financier-api', items);
-  app.use('/financier-api', users);
+  app.use('/financier_api', items);
+  app.use('/financier_api', users);
 };
