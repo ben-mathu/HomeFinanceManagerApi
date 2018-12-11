9f9987f6ebf7a00f2956950b46fb5c7ac2054b97
Benard Mathu <mathubenard@gmail.com>
2018-12-11 20:36:44 +0300

    Updates: updates

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 80,
+    port = process.env.PORT || 8080,
     mongoose = require('mongoose'),
     Item = require('./api/models/financeManagerModel'),
     User = require('./api/models/financierUserModel'),
