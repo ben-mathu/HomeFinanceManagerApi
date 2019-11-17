f50bf358fd407131d265369f8d8fb3da6fa1ad99
Benard Mathu <mathubenard@gmail.com>
2019-11-17 16:26:14 +0300

    update app version

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const express = require('express'),
 //and url connection
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://admin:JWMCEMHVPGQCUTOP@portal-ssl513-10.bmix-eu-gb-yp-8a0bb7ae-3707-4826-8a4c-c65f52d54179.1717821952.composedb.com:16447,portal-ssl505-38.bmix-eu-gb-yp-8a0bb7ae-3707-4826-8a4c-c65f52d54179.1717821952.composedb.com:16447/compose?authSource=admin&ssl=true', { useNewUrlParser: true});
-mongoose.connect('mongodb://localhost:27017/financeManagerDb', { useNewUrlParser: true}, { useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost:27017/financeManagerDb', { useNewUrlParser: true})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
