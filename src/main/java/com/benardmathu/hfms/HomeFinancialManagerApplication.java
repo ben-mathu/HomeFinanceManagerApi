7c62babb20be51ce954025b3b9743e43853ffbe4
Benard Mathu <mathubenard@gmail.com>
2021-03-26 02:06:21 +0300

    Update

---
+++ src/main/java/com/benardmathu/hfms/HomeFinancialManagerApplication.java
@@ -0,0 +1,15 @@
+package com.benardmathu.hfms;
+
+import com.benardmathu.hfms.init.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 25/03/2021
+ */
+@SpringBootApplication
+public class HomeFinancialManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeFinancialManagerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/benardmathu/hfms/data/grocery/GroceryRepository.java
@@ -0,0 +1,10 @@
+package com.benardmathu.hfms.data.grocery;
+
+import com.benardmathu.hfms.data.grocery.model.Grocery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created on 25/03/2021
+ */
+public interface GroceryRepository extends JpaRepository<Grocery, > {
+}
