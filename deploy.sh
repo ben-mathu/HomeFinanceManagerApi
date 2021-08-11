36a26ea99b9378572a91fbb485d9215b6f3ccb0a
Benard Mathu <mathubenard@gmail.com>
2021-08-11 22:45:22 +0300

    Remove compiled files

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mvn clean install
-rm $CATALINA_HOME/hfmsapp/ROOT.war
-mv target/hfms.war $CATALINA_HOME/hfmsapp/ROOT.war
-sudo service tomcat restart
-tail -fv $CATALINA_HOME/logs/catalina.out
--- target/hfms/WEB-INF/dashboard/dialogs/message_dialog.jsp
+++
@@ -1,5 +0,0 @@
-<div id="messageDialog" class="dialog message-style">
-    <div id="messageDialogContainer" class="message-container" >
-        <span id="messageReport"></span>
-    </div>
-</div>
